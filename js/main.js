4db9300192705a3613e8980ff3fdef7493939a99
Cue <cue@loadingchunks.net>
2013-01-12 03:06:04 +0000

    Added submodule for Metro UI Added basic template

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
+$(document).ready(function() {
+    $("#page-holder").css('margin-top', $("#page-title").height() - 50);
 
+    $(window).resize(function() {
+        $("#page-holder").css('margin-top', $("#page-title").height() - 50);
+    });
+});
